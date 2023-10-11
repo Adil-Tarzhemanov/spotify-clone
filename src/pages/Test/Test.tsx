19a59928f69646d3d70a60e6ada447d3d8a21160
chinakkk <chingizkeldibekov@gmail.com>
2023-10-11 16:12:39 +0600

    Created song component.

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,17 +1,33 @@
 import styles from './Test.module.scss';
 import { FC } from 'react';
 import WhiteButton from 'src/components/WhiteButton/WhiteButton';
+import CardMusic from 'src/components/CardMusic/CardMusic';
+import testImage from '/src/assets/testImg.png';
 
 type TestProps = {};
 
 const Test: FC = () => {
 	return (
 		<div className={styles.container}>
-			<div>
+			<div className={styles.horizontalBlock}>
 				<WhiteButton title={'Explore premium'} />
 				<WhiteButton title={'Create playlist'} />
 			</div>
-			<div>{/*<input type="text" placeholder={'himanyo'} />*/}</div>
+			<div className={styles.horizontalBlock}>
+				<div className={styles.verticalBlock}>
+					<CardMusic
+						title={"Today's top hits"}
+						text={'Olivia Rodrigo is on top of the Hottest 50!'}
+						imageURL={testImage}
+					/>
+				</div>
+				<div className={styles.verticalBlock}>
+					<CardMusic
+						title={'RapCaviar'}
+						text={'New music from Cardi B, Megan Thee Stallio…'}
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
